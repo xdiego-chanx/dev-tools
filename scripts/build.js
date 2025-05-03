3a696fcb0c7e67a33756025d76b483ccae94b431
Diego Chan <xdiego.chanx@gmail.com>
2025-05-03 02:02:01 -0500

    Setup directory aliases

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,20 @@
 import { build } from "esbuild";
+import alias from "esbuild-plugin-alias";
 import fs from "fs";
+import path from "path";
 
 const result = await build({
-  entryPoints: ["src/main/main.ts"],
-  bundle: true,
-  platform: "node",
-  format: "esm",
-  outfile: "dist/main.js"
+    entryPoints: ["src/main/main.ts"],
+    bundle: true,
+    platform: "node",
+    format: "esm",
+    outfile: "dist/main.js",
+    plugins: [
+        alias({
+            "@main": path.join(import.meta.dirname, "..", "src", "main"),
+            "@test": path.join(import.meta.dirname, "..", "src", "test"),
+        })
+    ]
 });
 
 const shebang = "#!/usr/bin/env node\n";
---
+++ src/test/cli/Command.spec.ts
@@ -0,0 +1,6 @@
+import { Command } from "@main/cli/Command";
+import { describe } from "node:test";
+
+describe("Command", () => {
+    
+});
